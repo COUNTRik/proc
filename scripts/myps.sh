855556355bea79ea2d4f6be39b28c8b3b7b2c696
Albert <countrik@yandex.ru>
2020-09-03 22:22:57 +0300

    add myps.sh

--- scripts/myps.sh
+++ scripts/myps.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Добавляем заголовки к столбцам 
-echo "PID PPID ST NAME" | awk '{printf "%-6s %-6s %-2s %-10s\n", $1, $2, $3, $4}'
+# Добавляем заголовки к столбцам
+echo "PID TTY ST TIME NAME" | awk '{printf "%-6s %-6s %-2s %-5s %s\n", $1, $2, $3, $4, $5}'
 
 # Проходим циклом все папки с процессами
 for path in /proc/[0-9]*
 do
 
 # Cохраняем данные с файла stat в переменную $stat
-# Примечание: таким же образом можно добавить данные о процессе
-# из других файлов в переменную, например cmd=$(cat $path/cmdline)
 stat=$(cat $path/stat | tr -d "(" | tr -d ")")
+cmd=$(cat $path/cmdline)
 
 # Выводим необходимые нам данные с помощью awk
-echo $stat | awk '{printf "%-6s %-6s %-2s %-10s\n", $1, $4 ,$3, $2}'
+# echo $stat | awk '{printf "%-6s %-6s %-2s %-10s\n", $1, $4 ,$3, $2}'
+echo $stat $cmd | awk '{printf "%-6s %-6s %-2s %-5s %s\n", $1, $4 ,$3, ($14 + $17)/100, $53}'
 done
